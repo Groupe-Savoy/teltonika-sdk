e9eae156925641692f0145b96a321605395708db
Emilien Leroy <eleroy@savoy-international.com>
2025-12-18 09:39:14 +0100

    💚 Fix vitest config alias

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    alias: {
+      '@/': new URL('./src/', import.meta.url).pathname, 
+    },
     coverage: {
       include: ['src/**/*.ts']
     },
